e2d07acab335eefb8072568769eb0f35a8cd2724
rainzz# <ffdubberke@gmail.com>
2025-11-24 19:29:30 +0100

    refactored files into src directory, added some missing types and fxied an error when omitting the mod directory

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const { gatherArchiveData, analyseArchives, getArchivePathsOfDirectory } = require('./archive.js')
-const { getArgs } = require('./args.js')
-const { writeResults } = require('./result-files.js')
+const { gatherArchiveData, analyseArchives, getArchivePathsOfDirectory } = require('./src/archive.js')
+const { getArgs } = require('./src/args.js')
+const { writeResults } = require('./src/result-files.js')
 
 const main = async () => {
-    const {
-        modDir,
-        withAutomatFiles,
-        showAllConflictingFiles,
-        modNamesOnly
-    } = getArgs()
+    try {
+        const {
+            modDir,
+            withAutomatFiles,
+            showAllConflictingFiles,
+            modNamesOnly
+        } = getArgs()
 
-    const modArchives = getArchivePathsOfDirectory(modDir)
-    const archiveData = await gatherArchiveData(modArchives)
-    const results     = await analyseArchives(archiveData)
+        const modArchives = getArchivePathsOfDirectory(modDir)
+        const archiveData = await gatherArchiveData(modArchives)
+        const results     = await analyseArchives(archiveData)
 
-    writeResults(results, showAllConflictingFiles, modNamesOnly, withAutomatFiles)
+        writeResults(results, showAllConflictingFiles, modNamesOnly, withAutomatFiles)
+    } catch (error) {
+        console.error('Unexpected error', error)
+        process.exit(1)
+    }
 }
 
 main()
